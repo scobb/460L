4371591f564b5f9b5738e3c5efad3447c0c0c443
Steve Cobb <scobb1@utexas.edu>
2014-09-22 10:53:18 -0500

    Added email to crontask

--- blog/Blog/src/blog/SendEmailServlet.java
+++ blog/Blog/src/blog/SendEmailServlet.java
@@ -2,7 +2,15 @@ package blog;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
+import java.util.logging.Logger;
 
+import javax.mail.Address;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,29 +20,58 @@ import blog.dao.SubscriberDAO;
 import blog.entity.BlogPost;
 import blog.entity.Subscriber;
 
-public class SendEmailServlet extends HttpServlet {
+public class SendEmailServlet extends HttpServlet
+{
+	public static final Logger _log = Logger.getLogger(SendEmailServlet.class
+			.getName());
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		// TODO: called by crontask - should send e-mail of all "new" posts to all subscribers, then mark all posts as not new
+			throws IOException
+	{
+		// TODO: called by crontask - should send e-mail of all "new" posts to all
+		// subscribers, then mark all posts as not new
 		List<Subscriber> subscribers = SubscriberDAO.INSTANCE.getSubscribers();
 		List<BlogPost> newPosts = BlogDAO.INSTANCE.getNewBlogPosts();
-		
+
 		String emailBody = "Hello subscriber,\nHere are the new blog posts from our travel blog:\n";
-		
-		for (BlogPost post: newPosts) { 
-			// TODO : do we actually want to send html here? or some plaintext version
+
+		for (BlogPost post : newPosts)
+		{
+			// TODO : do we actually want to send html here? or some plaintext
+			// version
 			emailBody += post.displayHtml();
-			
+
 			// persist that this blog post is no longer new
 			BlogDAO.INSTANCE.markAsNotNew(post);
 		}
-		
-		for (Subscriber subscriber: subscribers) {
+
+		for (Subscriber subscriber : subscribers)
+		{
 			// TODO: send email
+			Properties props = new Properties();
+			Session session = Session.getDefaultInstance(props, null);
+			try
+			{
+				MimeMessage message = new MimeMessage(session, req.getInputStream());
+				Address fromAddress = new InternetAddress(
+						"admin@cobbreynoldsblog.appspotmail.com");
+				Address toAddress = new InternetAddress(subscriber.getEmail()
+						.getName());
+				String subject = "New travel blog posts!";
+
+				// Send out Email
+				MimeMessage outMessage = new MimeMessage(session);
+				outMessage.setFrom(fromAddress);
+				outMessage.addRecipient(MimeMessage.RecipientType.TO, toAddress);
+				outMessage.setSubject(subject);
+				outMessage.setText(emailBody);
+				Transport.send(message);
+			} catch (MessagingException e)
+			{
+				_log.info("ERROR: Could not send out Email Results response : "
+						+ e.getMessage());
+			}
 		}
-		
-		
-		
 
 	}
 }
