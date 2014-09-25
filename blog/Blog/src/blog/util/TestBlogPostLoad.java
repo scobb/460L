8086a8b4c454de405af839468fcef85c257898fc
Steve Cobb <scobb1@utexas.edu>
2014-09-24 21:15:21 -0500

    Adding one i forgot

--- blog/Blog/src/blog/util/TestBlogPostLoad.java
+++ blog/Blog/src/blog/util/TestBlogPostLoad.java
@@ -5,6 +5,8 @@ import java.util.List;
 import blog.dao.BlogDAO;
 import blog.entity.BlogPost;
 
+import com.google.appengine.api.datastore.Text;
+
 /**
  * @author Steve Cobb scc2448 Sep 20, 2014
  * */
@@ -12,27 +14,27 @@ public class TestBlogPostLoad {
 	public static boolean addPosts() {
 		String author = "Steve Cobb";
 		String title = "Test Title 1";
-		String body = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+		Text body = new Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.");
 		Boolean post1 = BlogDAO.INSTANCE.saveBlogPost(author, title, body);
 		
 		author = "Jon Reynolds";
 		title = "Test Title 2";
-		body = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+		body = new Text("It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 		Boolean post2 = BlogDAO.INSTANCE.saveBlogPost(author, title, body);
 		
 		author = "Jon Reynolds";
 		title = "Test Title 3";
-		body = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+		body = new Text("It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 		Boolean post3 = BlogDAO.INSTANCE.saveBlogPost(author, title, body);
 		
 		author = "Jon Reynolds";
 		title = "Test Title 4";
-		body = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+		body = new Text("It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 		Boolean post4 = BlogDAO.INSTANCE.saveBlogPost(author, title, body);
 		
 		author = "Jon Reynolds";
 		title = "Test Title 5";
-		body = "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+		body = new Text("It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 		Boolean post5 = BlogDAO.INSTANCE.saveBlogPost(author, title, body);
 		
 		return post1 && post2 && post3 && post4 && post5;
